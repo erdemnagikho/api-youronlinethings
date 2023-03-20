1ab7896e7b54a709ffe074c0362e2ba47e16dd24
Erdem Nayir <erdemnayir@Erdems-MacBook-Pro.local>
2023-03-20 19:21:44 +0300

    Test on api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user', function (Request $request) {
+    return User::query()->first();
 });
