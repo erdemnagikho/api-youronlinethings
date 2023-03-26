bd1a629a8efd89ead5fbb40aef0e61fcbdc4e5f5
erdemnayir <erdemnayir@gmail.com>
2023-03-26 22:53:08 +0300

    Add job for testing user creating with factory

---
+++ app/Jobs/UserFactoryJob.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UserFactoryJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $user = User::factory()->create();
+        echo 'Event: User created with factory' . PHP_EOL;
+        echo json_encode($user->toArray()) . PHP_EOL;
+    }
+}
