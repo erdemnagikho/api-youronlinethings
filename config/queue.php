d7a56255fe23223792b207d1938a53ba85eb43eb
erdemnayir <erdemnayir@gmail.com>
2023-03-26 22:32:44 +0300

    Fix queue rabbitmq conf

--- config/queue.php
+++ config/queue.php
@@ -78,8 +78,8 @@ return [
                 [
                     'host' => env('RABBITMQ_HOST', '127.0.0.1'),
                     'port' => env('RABBITMQ_PORT', 5672),
-                    'user' => env('RABBITMQ_USER', 'guest'),
-                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'user' => env('RABBITMQ_USER', 'admin'),
+                    'password' => env('RABBITMQ_PASSWORD', '123456'),
                     'vhost' => env('RABBITMQ_VHOST', '/'),
                 ],
             ],
