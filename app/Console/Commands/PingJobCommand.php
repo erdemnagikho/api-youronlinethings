0137ab73b84c14e54bd4ebc4a946bc6172d60d9a
erdemnayir <erdemnayir@gmail.com>
2023-03-26 16:01:21 +0300

    Integration of RabbitMQ

---
+++ app/Console/Commands/PingJobCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\PingJob;
+use Illuminate\Console\Command;
+
+class PingJobCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ping:job';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        PingJob::dispatch();
+    }
+}
